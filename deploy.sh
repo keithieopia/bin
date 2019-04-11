27c3ec341fc77edb67f82af65021f9c5ebd06f77
Timothy Keith <timothykeith@gmail.com>
2019-04-11 19:00:28 -0400

    avoid creating temp copys of repo when possible

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,32 @@
 REPO="https://github.com/keithieopia/dotfiles.git"
 
 deployer() {
-	echo
-	echo "DOWNLOADING..."
-	echo "--------------"
-	git clone -b $BRANCH $REPO $TMPGITDIR
-	echo
-
-	if [ "$?" -eq 0 ]; then
-		cd $TMPGITDIR
-		git config status.showUntrackedFiles no
-	else
+	# BUG WORKAROUND:
+	# git ls-remote will unexpectedly fail if not in a readable directory
+	# even though it doesn't touch the local FS and only works with remote 
+	# repos. To fix, we cd into $HOME before running
+	cd ~
+	FIND_BRANCH=$(git ls-remote --heads $REPO $BRANCH 2>/dev/null | wc -l)
+	
+	if [ "$FIND_BRANCH" -ne 1 ]; then
+		git ls-remote --heads $REPO
 		echo "$BRANCH does not have any configuration available (git branch not found)"
-		rm -rf $TMPGITDIR
 		exit 1
 	fi
 
 	if [ -d "$TARGETDIR.git" ]; then
+		echo
+		echo "DOWNLOADING..."
+		echo "--------------"
+		TMPGITDIR=$(mktemp -d /tmp/$(basename $0).XXXXXX)
+		git clone --no-checkout -b $BRANCH $REPO $TMPGITDIR
+		cd $TMPGITDIR
+		git config status.showUntrackedFiles no
+
+		echo
 		echo "$TARGETDIR IS ALREADY UNDER VCS, CHECKING..."
 		echo "--------------------------------------------"
+		cd $TMPGITDIR
 		git remote add -f tmp $TARGETDIR >/dev/null 2>&1
 		git remote update >/dev/null 2>&1
 
@@ -36,33 +44,33 @@ deployer() {
 			echo "$TARGETDIR repo matches remote master repo, scrapping our downloaded copy"
 		fi
 	else
-		sudo mv -r $TMPGITDIR/.git $TARGETDIR
-		rm -rf $TMPGITDIR
-	fi
+		echo "DOWNLOADING..."
+		echo "--------------"
+		git clone --no-checkout -b $BRANCH $REPO $TARGETDIR
+		git config status.showUntrackedFiles no
 
-	cd /
-
-	if ! git status | grep -q "nothing to commit"; then
-		echo
-		echo "LOCAL CONFLICTING CHANGES FOUND!"
-		echo "--------------------------------"
-		echo "Some files differ from what's tracked in git, check the following"
-		echo "files as they will be overwritten: "
-		echo
-
-		git status | grep "modified:" | awk '{print $2}'
-		echo
-
-		echo -n "Overwrite the above files (y/n)? "
-		read answer
-
-		if [ "$answer" != "${answer#[Yy]}" ]; then
-			sudo git reset --hard
-		else
+		if ! git status | grep -q "nothing to commit"; then
 			echo
-			echo "You requested not to continue, so no files where changed"
-			echo "You can manually run 'cd $TARGETDIR && git reset --hard' as root later if you wish"
-			exit 1
+			echo "LOCAL CONFLICTING CHANGES FOUND!"
+			echo "--------------------------------"
+			echo "Some files differ from what's tracked in git, check the following"
+			echo "files as they will be overwritten: "
+			echo
+
+			git status | grep "modified:" | awk '{print $2}'
+			echo
+
+			echo -n "Overwrite the above files (y/n)? "
+			read answer
+
+			if [ "$answer" != "${answer#[Yy]}" ]; then
+				sudo git reset --hard
+			else
+				echo
+				echo "You requested not to continue, so no files where changed"
+				echo "You can manually run 'cd $TARGETDIR && git reset --hard' as root later if you wish"
+				exit 1
+			fi
 		fi
 	fi
 }
@@ -72,7 +80,6 @@ echo "## DEPLOYING SYSTEM CONFIGS"
 echo "############################################################"
 
 BRANCH=$(hostname)
-TMPGITDIR=$(mktemp -d /tmp/$(basename $0).XXXXXX)
 TARGETDIR="/"
 deployer
 
@@ -82,6 +89,5 @@ echo "## DEPLOYING DOTFILES"
 echo "############################################################"
 
 BRANCH="master"
-TMPGITDIR=$(mktemp -d /tmp/$(basename $0).XXXXXX)
 TARGETDIR="$HOME/"
 deployer
